4a40eb664cd06899272dba46c4c8d2aea716acb7
TeddyWChoi <teddy.w.choi@gmail.com>
2025-10-27 02:32:53 +0900

    Set base path to /eerion/ for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
-    base: '/',
+    base: '/eerion/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
